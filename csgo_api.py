58fa91e100fe05eddd2d5dffa91c097e27013458
Søren Howe Gersager <syrelyre@gmail.com>
2016-08-09 23:09:13 +0200

    implementation now scrapes all matches

--- csgo_api.py
+++ csgo_api.py
@@ -1,35 +1,68 @@
 #!/usr/bin/env python3
-import pymongo
-import json
-import flask
-import os
+# -*- coding: utf-8 -*-
+"""
+Flask REST API for showing CSGOLounge matches
+"""
 
-from flask import Flask, request, jsonify
+import json
+import os
+import pymongo
+from flask import Flask, request, jsonify, make_response, url_for, redirect
+
 app = Flask(__name__)
-app.debug = False
 
 
 # reading config file with DB_HOSTNAME and DB_PORT variables
-with open(os.path.join(os.path.dirname(__file__),"settings.txt"),"r") as f:
-    config = f.readlines()
+with open(os.path.join(os.path.dirname(__file__), "settings.txt"), "r") as f:
+    CONFIG = f.readlines()
 
-config_dict = dict(line.strip().split("=") for line in config if not line.startswith("#"))
-DB_HOSTNAME=config_dict["DB_HOSTNAME"]
-DB_PORT=int(config_dict["DB_PORT"])
+CONFIG_DICT = dict(line.strip().split("=") for line in CONFIG if not line.startswith("#"))
 
-try:
-    client = pymongo.MongoClient(DB_HOSTNAME, DB_PORT)
-except pymongo.errors.ConnectionFailure:
-    print("failed to connect to database")
-    sys.exit()
-db = client["csgo"]
-collection = db["csgomatches"]
+DB_HOSTNAME = CONFIG_DICT["DB_HOSTNAME"]
+DB_PORT = int(CONFIG_DICT["DB_PORT"])
+
+CLIENT = pymongo.MongoClient(DB_HOSTNAME, DB_PORT)
+DB = CLIENT["csgo"]
+MATCHES = DB["csgomatches"]
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
+def index():
+    """
+    default endpoint
+    """
+    return redirect(url_for("get_matches"))
+
+
+@app.route('/matches', methods=["GET"])
 def get_matches():
-	result = collection.find().sort([("_id", pymongo.DESCENDING)])
-	return jsonify({"matches": list(result)})
+    """
+    Returns all matches matching the parameters given
+    """
+    allowed_fields = ["teama", "teamb", "winner", "completed",
+                      "team_a_odd", "team_b_odd"]
+    sort_fields = {key: value for key, value in request.args.items() if key in allowed_fields}
+
+    result = MATCHES.find(sort_fields).sort([("_id", pymongo.DESCENDING)])
+    return jsonify({"matches": list(result)})
+
+
+@app.route("/matches/<int:match_id>", methods=["GET"])
+def get_match(match_id):
+    """
+    Returns a single match with specified ID
+    """
+    result = MATCHES.find({"_id": match_id}).limit(1)[0]
+    return jsonify({"match": result})
+
+
+@app.errorhandler(404)
+def page_not_found():
+    """
+    error handler for 'Not Found'
+    returns jsonified error response
+    """
+    return make_response(jsonify({"error": "Endpoint not found"}), 404)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=5050, debug=True)
--- csgo_match_scraper.py
+++ csgo_match_scraper.py
@@ -1,123 +1,136 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module containing functions for scraping the matches of CSGOLounge.
+"""
+import os
+import time
 import pymongo
 import bs4
 import requests
 import timestring
-import os
 
-def scrape_frontpage_match_urls(base_url):
-    response = requests.get(base_url)
-    if (response.status_code != 200):
-        print("error, HTTP status code not 200")
-        return
+# reading config file with DB_HOSTNAME and DB_PORT variables
+CURRENT_DIR = os.path.join(os.path.dirname(__file__))
+with open(os.path.join(CURRENT_DIR, "settings.txt"), "r") as f:
+    CONFIG = f.readlines()
 
-    soup = bs4.BeautifulSoup(response.text)
-    frontpage_matches = soup.find_all("div", {"class": "match"})
-    match_urls = []
+CONFIG_DICT = dict(line.strip().split("=") for line in CONFIG if not line.startswith("#"))
+DB_HOSTNAME = CONFIG_DICT["DB_HOSTNAME"]
+DB_PORT = int(CONFIG_DICT["DB_PORT"])
 
-    for match in frontpage_matches:
-        completed_status = False
-        
-        if "notavailable" in match["class"]:
-            completed_status = True
-        link = match.find("a")
-        if link:
-            match_urls.append((link["href"], completed_status))
-    return match_urls
+# initialise database connection
+CLIENT = pymongo.MongoClient(DB_HOSTNAME, DB_PORT)
 
-def scrape_latest_csgolounge_matches(base_url):
-    match_urls = scrape_frontpage_match_urls(base_url)
-    matches = []
-    for url in match_urls:
-        response = requests.get(CSGOLOUNGE_BASE_URL+url[0])
-        if (response.status_code != 200):
-            print("error, HTTP status code not 200 for url: {}".format(url[0]))
+DB = CLIENT["csgo"]
+MATCHES = DB["csgomatches"]
+
+CSGOLOUNGE_BASE_URL = "http://csgolounge.com/"
+
+
+def scrape_all_matches(start_id=7000, time_delay=1):
+    """
+    Scrapes all completed CSGOLounge matches
+    starting at start_id and incrementing
+    uses a time_delay to avoid a large number
+    of requests within a short time period
+    """
+    reached_newest = False
+    while not reached_newest:
+        response = requests.get(CSGOLOUNGE_BASE_URL+"match?m={}".format(start_id))
+        start_id += 1
+        # TODO - should we retry for X passes?
+        if response.status_code != 200:
+            print("returned response {}".format(response.status_code))
             continue
+        # need extra check because CSGOLounge does not return HTTP 404 on /404 -_-
+        if response.url == "https://csgolounge.com/404":
+            reached_newest = True
+            print("returned page 404")
+        # don't include pages that redirect to predict pages
+        if response.url.startswith("https://csgolounge.com/predict"):
+            print("was predict page")
+            continue
+        if is_match_with_winner(response):
+            match_data = extract_match_data(response)
+            MATCHES.save(match_data)
+            print(match_data)
+        time.sleep(time_delay)
+        print("sleeping")
 
-        soup = bs4.BeautifulSoup(response.text)
-        box = soup.find("div", {"class": "box-shiny-alt"})
-        team_a = box.find_all("span")[0].find("b").text
-        team_a_odds = box.find_all("span")[0].find("i").text
-        team_b = box.find_all("span")[2].find("b").text
-        team_b_odds = box.find_all("span")[2].find("i").text
 
-        full = box.find("div", {"class": "full"})
-        reward_boxes = full.find_all("div",{"class":"half"})
-        # if noone has played on both teams, no reward can be given
-        if (team_a_odds == "0%" and team_b_odds == "0%"):
-            team_a_reward = ""
-            team_b_reward = ""
-        else:
-            print(reward_boxes[0].div.br.contents[0])
-            team_a_reward = reward_boxes[0].div.br.contents[0].split("for")[0].strip()
-            team_b_reward = reward_boxes[1].div.br.contents[0].split("for")[0].strip()
-        
-        completed = url[1]
-        if ("(win)" in team_a):
-            team_a = team_a.replace(" (win)", "")
-            winner = team_a
-        elif ("(win)" in team_b):
-            team_b = team_b.replace(" (win)", "")
-            winner = team_b
-        else:
-            winner = "_NONE_"
+def is_match_with_winner(response):
+    """
+    Determines if a winner has been found
+    or not (and thus completed)
+    """
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    box = soup.find("div", {"class": "box-shiny-alt"})
 
-        status_text = ""
-        box_children = box.find_all("div", recursive=False)
-        if len(list(box_children[1].findChildren())) == 0:
-            status_text = box_children[1].text.strip()
+    team_a = box.find_all("span")[0].find("b").text
+    team_b = box.find_all("span")[2].find("b").text
+    win_str = "(win)"
 
-        options_boxes = box.find_all("div",{"class":"half"})
-        BO = options_boxes[1].text
-        hour = options_boxes[2].text.strip()
-        date = options_boxes[2]["title"]
-        match_id = int(url[0].split("=")[1])
-        print(match_id)
-        timestring_date = timestring.Date("{1} at {0}".format(date, hour[:5]))
-        print(BO)
-        print(team_a, team_a_odds, team_a_reward, team_b, team_b_odds, team_b_reward, "winner={}".format(winner))
-        print("\n")
+    return win_str in team_a or win_str in team_b
 
-        match = {"_id": match_id,
-             "match_date": str(timestring_date), 
-             "match_link": url[0],
-             "team_a_potential_reward": team_a_reward,
-             "team_b_potential_reward": team_b_reward,
-             "team_a": team_a,
-             "team_b": team_b,
-             "team_a_odd":team_a_odds,
-             "team_b_odd": team_b_odds,
-             "match_type": BO,
-             "winner": winner,
-             "completed": completed,
-             "status_text": status_text}
-        matches.append(match)
-    return matches
+
+def extract_match_data(response):
+    """
+    Extracts the match information from a CSGOLounge
+    match site e.g. https://csgolounge.com/match?m=7000
+    """
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    box = soup.find("div", {"class": "box-shiny-alt"})
+    team_a = box.find_all("span")[0].find("b").text
+    team_a_odds = box.find_all("span")[0].find("i").text
+    team_b = box.find_all("span")[2].find("b").text
+    team_b_odds = box.find_all("span")[2].find("i").text
+
+    full = box.find("div", {"class": "full"})
+    reward_boxes = full.find_all("div", {"class": "half"})
+    # if noone has played on both teams, no reward can be given
+    if team_a_odds == "0%" and team_b_odds == "0%":
+        team_a_reward = ""
+        team_b_reward = ""
+    else:
+        print(reward_boxes[0].div.br.contents[0])
+        team_a_reward = reward_boxes[0].div.br.contents[0].split("for")[0].strip()
+        team_b_reward = reward_boxes[1].div.br.contents[0].split("for")[0].strip()
+
+    if "(win)" in team_a:
+        team_a = team_a.replace(" (win)", "")
+        winner = team_a
+    elif "(win)" in team_b:
+        team_b = team_b.replace(" (win)", "")
+        winner = team_b
+
+    status_text = ""
+    box_children = box.find_all("div", recursive=False)
+    if len(list(box_children[1].findChildren())) == 0:
+        status_text = box_children[1].text.strip()
+
+    options_boxes = box.find_all("div", {"class": "half"})
+    match_type = options_boxes[1].text
+    hour = options_boxes[2].text.strip()
+    date = options_boxes[2]["title"]
+    match_id = int(response.url.split("=")[1])
+    timestring_date = timestring.Date("{1} at {0}".format(date, hour[:5]))
+
+    match_data = {"_id": match_id,
+                  "match_date": str(timestring_date),
+                  "match_link": response.url,
+                  "team_a_potential_reward": team_a_reward,
+                  "team_b_potential_reward": team_b_reward,
+                  "team_a": team_a,
+                  "team_b": team_b,
+                  "team_a_odd": team_a_odds,
+                  "team_b_odd": team_b_odds,
+                  "match_type": match_type,
+                  "winner": winner,
+                  "status_text": status_text}
+    return match_data
 
 if __name__ == "__main__":
-    # reading config file with DB_HOSTNAME and DB_PORT variables
-    with open(os.path.join(os.path.dirname(__file__),"settings.txt"),"r") as f:
-        config = f.readlines()
-
-    config_dict = dict(line.strip().split("=") for line in config if not line.startswith("#"))
-    DB_HOSTNAME=config_dict["DB_HOSTNAME"]
-    DB_PORT=int(config_dict["DB_PORT"])
-
-    CSGOLOUNGE_BASE_URL = "http://csgolounge.com/"
-
-    # initialise database connection
-    try:
-        client = pymongo.MongoClient(DB_HOSTNAME, DB_PORT)
-    except pymongo.errors.ConnectionFailure:
-        print("failed to connect to database")
-        sys.exit()
-
-    db = client["csgo"]
-    collection = db["csgomatches"]
+    scrape_all_matches()
     #latest_match = collection.find().sort("_id", direction=pymongo.DESCENDING).limit(1)[0]
-    matches = scrape_latest_csgolounge_matches(CSGOLOUNGE_BASE_URL)
     
-    for match in matches:
-        collection.save(match)
-
-
